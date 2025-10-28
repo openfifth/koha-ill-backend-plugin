bc35b78c0cf072c6c5de5bef44b0bb0dcb89f820
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2025-10-28 14:38:50 -0100

    Add 41111 support

--- Koha/Plugin/Com/PTFSEurope/PluginBackend/Api.pm
+++ Koha/Plugin/Com/PTFSEurope/PluginBackend/Api.pm
@@ -15,33 +15,58 @@ sub ill_backend_availability {
     # Wait 2 seconds to simulate a real request to a third-party provider
     sleep(2);
 
-    my $metadata = decode_json( decode_base64( uri_unescape( $controller->validation->param('metadata') || '' ) ) );
+    my $metadata = decode_json(
+        decode_base64(
+            uri_unescape( $controller->validation->param('metadata') || '' )
+        )
+    );
 
-    # Example of missing required metadata for availability check
-    unless ( $metadata->{doi} || $metadata->{pubmedid} ) {
-        return $controller->render(
-            status  => 400,
-            openapi => {
-                error => 'Missing required ISBN input data',
-            }
-        );
+    my $title = $metadata->{title};
+
+    # If title is defined, handle specific logic
+    if (defined $title) {
+
+        if (lc $title eq 'yellow') {
+            return $controller->render(
+                status  => 200,
+                openapi => {
+                    warning => 'Can be placed but will go through manual verification',
+                }
+            );
+
+        } elsif (lc $title eq 'red') {
+            return $controller->render(
+                status  => 400,
+                openapi => {
+                    error => 'Missing required ISBN input data',
+                }
+            );
+
+        } elsif (lc $title eq 'green') {
+            return $controller->render(
+                status  => 200,
+                openapi => {
+                    success => '',
+                }
+            );
+        }
     }
 
-    # 50% of the time will return success
-    if ( rand(100) >= 50){
+    my $rand = rand(100);
+    if ( $rand < 33.3 ) {
         return $controller->render(
             status  => 200,
-            openapi => {
-                success => '',
-            }
+            openapi => { success => '' },
         );
-    # 50% of the time will return unavailable
-    }else{
+    } elsif ( $rand < 66.6 ) {
+        return $controller->render(
+            status  => 200,
+            openapi => { warning => 'Can be placed but will go through manual verification' },
+        );
+    } else {
         return $controller->render(
             status  => 404,
-            openapi => {
-                error => 'Provided ISBN is not available in PluginBackend',
-            }
+            openapi => { error => 'Provided ISBN is not available in PluginBackend' },
         );
     }
 }
